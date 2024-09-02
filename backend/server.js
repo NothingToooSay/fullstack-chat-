dc21e1ac802bfb2dc6bc29a89078289eb096ed9b
NothingToooSay <ilya.samoha88@gmail.com>
2024-09-02 21:46:57 +0200

    realtime-chat install
    install server /express/

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    // root route http://localhost:5000
+    res.send("Hello world!!!")
+})
+
+app.listen(PORT, () => console.log(`Server running on this port ${PORT}`));
